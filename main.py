5898b715900f7736409b0ea24a51fa43e1f353af
Carlos Alves <carlos.alves.one@gmail.com>
2023-01-15 13:11:06 +0000

    Declare class neural network

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+
 # Goldsmiths University of London
 # Author....: Carlos Manuel de Oliveira Alves
 # Student...: cdeol003
 # Created...: 15/01/2023
 # FYP.......: NeuroCredit
 
+# neural network class definition
+class neuralNetwork:
 
+    
