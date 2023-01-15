e64b4728b6e812d4f14a47d4f06f1795102fab77
Carlos Alves <carlos.alves.one@gmail.com>
2023-01-15 13:27:12 +0000

    Create instance of neural network

--- main.py
+++ main.py
@@ -9,14 +9,14 @@
 class neuralNetwork:
 
     # initialise the neural network
-    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+    def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
         # set number of nodes in each input, hidden, output layer
-        self.inodes = inputnodes
-        self.hnodes = hiddennodes
-        self.onodes = outputnodes
+        self.inodes = input_nodes
+        self.hnodes = hidden_nodes
+        self.onodes = output_nodes
 
         # learning rate
-        self.lr = learningrate
+        self.lr = learning_rate
     
         pass
 
@@ -28,10 +28,13 @@ class neuralNetwork:
     def query():
         pass
     
-    # declare the number of input, hidden and output nodes
-    input_nodes = 3
-    hidden_nodes = 3
-    output_nodes = 3
+# declare the number of input, hidden and output nodes
+input_nodes  = 3
+hidden_nodes = 3
+output_nodes = 3
 
-    # declare the learning rate is 0.3
-    learning_rate = 0.3
+# declare the learning rate is 0.3
+learning_rate = 0.3
+
+# create instance of neural network
+n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
