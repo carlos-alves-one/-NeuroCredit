adbdef6d3578b78ec783ae22edfa9b00e15ed67d
Carlos Alves <carlos.alves.one@gmail.com>
2023-01-21 13:32:47 +0000

    Calculate signals into final output layer

--- main.py
+++ main.py
@@ -78,6 +78,9 @@ class neuralNetwork:
 
         # calculate the signals emerging from hidden layer
         hidden_outputs = self.activation_function(hidden_inputs)
+
+        # calculate signals into final output layer
+        final_inputs = np.dot(self.who, hidden_outputs)
         pass
     
 # declare the number of input, hidden and output nodes
